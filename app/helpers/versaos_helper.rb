585f5860ec57cc98892c7e94686d9810069e8c80
Samuel <mucabatera@gmail.com>
2024-05-30 14:25:36 -0300

    Acertando a view de escalas com helpers de versões

--- app/helpers/versaos_helper.rb
+++ app/helpers/versaos_helper.rb
@@ -4,4 +4,19 @@ module VersaosHelper
         musica = Musica.find(id)
         musica.nome
     end
+
+    def versao_da_musica(musica_id)
+        versao = Versao.where(musica_id: musica_id)
+        versao.nome
+    end
+
+    def versao_da_musica_escala(musica_id, versao_id)
+        versao = Versao.where(musica_id: musica_id, id: versao_id).first
+        versao&.nome
+    end
+
+    def link_versao_da_musica_escala(musica_id, versao_id)
+        versao = Versao.where(musica_id: musica_id, id: versao_id).first
+        versao&.link_youtube
+    end
 end
